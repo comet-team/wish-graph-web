80368b89f68179f16432994f6867af3bd117b3c6
Egor Molchan <90382636+egobars@users.noreply.github.com>
2022-04-21 15:02:14 +0300

    Init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var request = require('request')
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {need_print: false, list_url: []});
+});
+
+async function drawPage(req, res) {
+  let owner = req.body.owner;
+  var lst = [];
+  let promise_info = new Promise((resolve, reject) => {
+    request('https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM%3A' + owner, (err, response, body) => {
+      if (err) { return console.log(err); }
+      var p_body = JSON.parse(body);
+      let arr = [];
+      for (let i = 0; i < p_body.items.length; ++i) {
+        if (p_body.items[i].meta != undefined && p_body.items[i].meta.content != undefined) {
+          if (p_body.items[i].meta.content[0] != undefined) {
+            arr.push(p_body.items[i].meta.content[0].url);
+          }
+        }
+      }
+      resolve(arr);
+    });
+  });
+  lst = await promise_info;
+  res.render('index', {need_print: true, list_url: lst});
+}
+
+router.post('/', function(req, res, next) {
+  drawPage(req, res);
 });
 
 module.exports = router;
