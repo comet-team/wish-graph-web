cfa8beaffbd89e15fcc6e2ed83ae5908f668138e
Egor Molchan <90382636+egobars@users.noreply.github.com>
2022-04-22 14:55:59 +0300

    Added refs

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var request = require('request')
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', {need_print: false, list_url: []});
+  res.render('index', {need_print: false, list_url: [], list_id: []});
 });
 
 async function drawPage(req, res) {
   let owner = req.body.owner;
   var lst = [];
+  var id_arr = []
   let promise_info = new Promise((resolve, reject) => {
     request('https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM%3A' + owner, (err, response, body) => {
       if (err) { return console.log(err); }
@@ -18,6 +19,14 @@ async function drawPage(req, res) {
         if (p_body.items[i].meta != undefined && p_body.items[i].meta.content != undefined) {
           if (p_body.items[i].meta.content[0] != undefined) {
             arr.push(p_body.items[i].meta.content[0].url);
+            let id = p_body.items[i].id;
+            let idx = 0;
+            while (id[idx] != ':') {
+              ++idx;
+            }
+            ++idx;
+            id = id.slice(idx);
+            id_arr.push(id);
           }
         }
       }
@@ -25,7 +34,7 @@ async function drawPage(req, res) {
     });
   });
   lst = await promise_info;
-  res.render('index', {need_print: true, list_url: lst});
+  res.render('index', {need_print: true, list_url: lst, list_id: id_arr});
 }
 
 router.post('/', function(req, res, next) {
